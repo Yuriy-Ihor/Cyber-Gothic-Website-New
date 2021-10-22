0c93b833d43f1205f185af3dac021bd6deac6ca3
Oleg-Smal-git <olehsmal2002@gmail.com>
2021-10-23 00:16:34 +0300

    Minor fixes.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './styles/App.css';
 import './styles/parallax.css';
 import './styles/lens-flare.css';
 // import LensBlinkJSX from './components/lens-blink';
-import {despawnAll, spawnText, /* despawnText, despawnAll */ } from './components/text-animation'
+import {spawnText, despawnAll} from './components/text-animation';
+import {deviceType} from './components/device-type';
 
 window.onload = function() {
   despawnAll();
@@ -15,15 +16,16 @@ window.onload = function() {
 
 function App() {
 
-  // spawnText('text-block-1');
-  // despawnAll();
+  const groupStyle = {
+    height: deviceType() == "mobile" ? window.innerHeight : "100vh"
+  };
 
 	return (
 
   <div className="main">
 
     <div className="parallax">
-      <div className="parallax-group">
+      <div className="parallax-group" style={groupStyle}>
 
         <div className="parallax-layer parallax-layer--back">
           <img className="background" src="./media/background3.png" alt="" />
@@ -31,18 +33,6 @@ function App() {
 
         <div className="parallax-layer parallax-layer--back">
           <div className="text" id="text-block-1">
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
-              Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
-              Cras efficitur lacinia laoreet. 
-              Suspendisse a felis in libero vulputate varius non eget risus. 
-              Mauris congue lorem eu tortor vulputate, nec semper est ultricies. 
-              Curabitur ultricies purus enim, ut facilisis lectus ornare ac. 
-              Phasellus eget finibus lacus. 
-              Praesent commodo dictum urna, a blandit dui dignissim vel. 
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
@@ -55,7 +45,7 @@ function App() {
               Praesent commodo dictum urna, a blandit dui dignissim vel. 
               Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
             </p>
-            <p>
+            {/* <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
               Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
@@ -66,36 +56,12 @@ function App() {
               Phasellus eget finibus lacus. 
               Praesent commodo dictum urna, a blandit dui dignissim vel. 
               Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
-              Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
-              Cras efficitur lacinia laoreet. 
-              Suspendisse a felis in libero vulputate varius non eget risus. 
-              Mauris congue lorem eu tortor vulputate, nec semper est ultricies. 
-              Curabitur ultricies purus enim, ut facilisis lectus ornare ac. 
-              Phasellus eget finibus lacus. 
-              Praesent commodo dictum urna, a blandit dui dignissim vel. 
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
+            </p> */}
           </div>
         </div>
 
         <div className="parallax-layer parallax-layer--back">
           <div className="text" id="text-block-2">
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
-              Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
-              Cras efficitur lacinia laoreet. 
-              Suspendisse a felis in libero vulputate varius non eget risus. 
-              Mauris congue lorem eu tortor vulputate, nec semper est ultricies. 
-              Curabitur ultricies purus enim, ut facilisis lectus ornare ac. 
-              Phasellus eget finibus lacus. 
-              Praesent commodo dictum urna, a blandit dui dignissim vel. 
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
@@ -108,7 +74,7 @@ function App() {
               Praesent commodo dictum urna, a blandit dui dignissim vel. 
               Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
             </p>
-            <p>
+            {/* <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
               Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
@@ -119,36 +85,12 @@ function App() {
               Phasellus eget finibus lacus. 
               Praesent commodo dictum urna, a blandit dui dignissim vel. 
               Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
-              Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
-              Cras efficitur lacinia laoreet. 
-              Suspendisse a felis in libero vulputate varius non eget risus. 
-              Mauris congue lorem eu tortor vulputate, nec semper est ultricies. 
-              Curabitur ultricies purus enim, ut facilisis lectus ornare ac. 
-              Phasellus eget finibus lacus. 
-              Praesent commodo dictum urna, a blandit dui dignissim vel. 
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
+            </p> */}
           </div>
         </div>
 
         <div className="parallax-layer parallax-layer--back">
           <div className="text" id="text-block-3">
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
-              Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
-              Cras efficitur lacinia laoreet. 
-              Suspendisse a felis in libero vulputate varius non eget risus. 
-              Mauris congue lorem eu tortor vulputate, nec semper est ultricies. 
-              Curabitur ultricies purus enim, ut facilisis lectus ornare ac. 
-              Phasellus eget finibus lacus. 
-              Praesent commodo dictum urna, a blandit dui dignissim vel. 
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
@@ -161,7 +103,7 @@ function App() {
               Praesent commodo dictum urna, a blandit dui dignissim vel. 
               Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
             </p>
-            <p>
+            {/* <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
               Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
@@ -172,36 +114,35 @@ function App() {
               Phasellus eget finibus lacus. 
               Praesent commodo dictum urna, a blandit dui dignissim vel. 
               Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam orci est, dictum consectetur libero vel, blandit aliquet magna. 
-              Mauris eget consectetur ipsum. Etiam bibendum imperdiet tempor. 
-              Cras efficitur lacinia laoreet. 
-              Suspendisse a felis in libero vulputate varius non eget risus. 
-              Mauris congue lorem eu tortor vulputate, nec semper est ultricies. 
-              Curabitur ultricies purus enim, ut facilisis lectus ornare ac. 
-              Phasellus eget finibus lacus. 
-              Praesent commodo dictum urna, a blandit dui dignissim vel. 
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
+            </p> */}
+            <a className="play-button">Play Demo</a>
           </div>
         </div>
 
         <div className="parallax-layer parallax-layer--back">
           <header className="header">
-            <ul>
-              <li><img href="#" src="./media/logo.png" alt="" /></li>
-              <li><a href="#" onClick={() => {
-                spawnText('text-block-1');
-              }}>NFT</a></li>
-              <li><a href="#" onClick={() => {
-                spawnText('text-block-2');
-              }}>About The Game</a></li>
-              <li><a href="#" onClick={() => {
-                spawnText('text-block-3');
-              }}>Play Demo</a></li>
-            </ul>
+            {
+                deviceType() == "mobile" ? 
+
+                <ul>
+                  <li><img href="#" src="./media/logo.png" alt="" /></li>
+                </ul> 
+
+                :
+
+                <ul>
+                  <li><img href="#" src="./media/logo.png" alt="" /></li>
+                  <li><a className="desktop" href="#" onClick={() => {
+                    spawnText('text-block-1');
+                  }}>NFT</a></li>
+                  <li><a className="desktop" href="#" onClick={() => {
+                    spawnText('text-block-2');
+                  }}>About The Game</a></li>
+                  <li><a className="mobile" href="#" onClick={() => {
+                    spawnText('text-block-3');
+                  }}>Play Demo</a></li>
+                </ul>
+            }
           </header>
         </div>
 
