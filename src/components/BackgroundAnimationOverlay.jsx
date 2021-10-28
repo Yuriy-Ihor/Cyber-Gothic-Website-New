078bb19ef69d5de0beaae6be71a3521fa5409a7d
Oleg-Smal-git <olehsmal2002@gmail.com>
2021-10-28 21:36:35 +0300

    Midway update.

--- src/components/BackgroundAnimationOverlay.jsx
+++ src/components/BackgroundAnimationOverlay.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import { DeviceType } from './DeviceType';
 import { BackgroundSize } from './DynamicBackground';
 
@@ -14,6 +15,22 @@ function GenerateBackgroundAimationOverlay() {
     const backgroundStyle = BackgroundSize(); 
 
     return class extends Component {
+        constructor(props) {
+            super(props);
+
+            this.canvas = null;
+            this.context = null;
+            this.width = null;
+            this.height = null;
+
+            this.imageWidth = null;
+            this.imageHeight = null;
+            this.starCenterX = null;
+            this.starCenteerY = null;
+
+            this.animate = this.animate.bind(this);
+        }
+
         render() {
             return(
                 <canvas id="background-animation"></canvas>
@@ -21,11 +38,20 @@ function GenerateBackgroundAimationOverlay() {
         }
 
         componentDidMount() {
+            this.canvas = document.getElementById("background-animation");
+            this.context = this.canvas.getContext("2d");
+            this.width = this.canvas.getAttribute("width");
+            this.height = this.canvas.getAttribute("height");
+
             this.animate();
         }
 
         animate() {
+            this.context.clearRect(0, 0, this.width, this.height);
 
+
+
+            requestAnimationFrame(this.animate);
         }
     }
 }
